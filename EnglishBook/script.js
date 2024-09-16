433105ffa6d5fd6649084a40e79d89cb886c7b9e
mmahdin <mahdi9roozi@gmail.com>
2024-09-16 16:06:02 +0330

    add gpt voice (download the text to speech of gpt)

--- EnglishBook/script.js
+++ EnglishBook/script.js
@@ -61,4 +61,17 @@ document.addEventListener('DOMContentLoaded', () => {
   updateProgress('audio1', 'progressBar1', 'currentTime1', 'duration1');
   updateProgress('audio2', 'progressBar2', 'currentTime2', 'duration2');
   updateProgress('audio3', 'progressBar3', 'currentTime3', 'duration3');
+  updateProgress('audio4', 'progressBar4', 'currentTime4', 'duration4');
+  updateProgress('audio5', 'progressBar5', 'currentTime5', 'duration5');
+  updateProgress('audio6', 'progressBar6', 'currentTime6', 'duration6');
+  updateProgress('audio7', 'progressBar7', 'currentTime7', 'duration7');
+  updateProgress('audio8', 'progressBar8', 'currentTime8', 'duration8');
+  updateProgress('audio9', 'progressBar9', 'currentTime9', 'duration9');
+  updateProgress('audio10', 'progressBar10', 'currentTime10', 'duration10');
+  updateProgress('audio11', 'progressBar11', 'currentTime11', 'duration11');
+  updateProgress('audio12', 'progressBar12', 'currentTime12', 'duration12');
+  updateProgress('audio13', 'progressBar13', 'currentTime13', 'duration13');
+  updateProgress('audio14', 'progressBar14', 'currentTime14', 'duration14');
+  updateProgress('audio15', 'progressBar15', 'currentTime15', 'duration15');
+  updateProgress('audio16', 'progressBar16', 'currentTime16', 'duration16');
 });
